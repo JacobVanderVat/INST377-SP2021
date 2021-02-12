3b25f0ab54b991e439eb4af4eaf077deef560362
JacobVanderVat <jacobvandervat@gmail.com>
2021-02-12 14:37:14 -0500

    Fixed Array

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,19 +1,35 @@
-    let width = 130; 
-    let count = 4; 
+let width = 130;
+let count = 4;
+let list = carousel.querySelector("ul");
+let listItems = carousel.querySelectorAll("li");
+let imageArray = Array.from(listItems);
+let position = 0;
 
-    let list = carousel.querySelector('ul');
-    let listItems = carousel.querySelectorAll('li');
+imageArray.forEach((element) => {
+  element.style.position = "relative";
+});
 
-    let position = 0; 
+function movePrev() {
+  position += width * count;
+  position = Math.min(position, 0);
+  list.style.marginLeft = position + "px";
+}
 
-    carousel.querySelector('.prev').onclick = function() {
-      position += width * count;
-      position = Math.min(position, 0)
-      list.style.marginLeft = position + 'px';
-    };
+function moveNext() {
+  position -= width * count;
+  position = Math.max(position, -width * (listItems.length - count));
+  list.style.marginLeft = position + "px";
+}
 
-    carousel.querySelector('.next').onclick = function() {
-      position -= width * count;
-      position = Math.max(position, -width * (listItems.length - count));
-      list.style.marginLeft = position + 'px';
-    };
+function listner() {
+  const backArrow = document.querySelector(".prev");
+  const forwardArrow = document.querySelector(".next");
+  backArrow.addEventListener("click", (event) => {
+    movePrev();
+  });
+  forwardArrow.addEventListener("click", (event) => {
+    moveNext();
+  });
+}
+
+window.onload = listner;
