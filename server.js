d0bd610f6f026bc3cb564489f01f677e4422f449
JacobVanderVat <jacobvandervat@gmail.com>
2021-02-18 19:20:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -40,13 +40,14 @@ app.route('/api')
     console.log('GET request detected');
     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
     const json = await data.json();
-    console.log('data from fetch', json);
+    // console.log('data from fetch', json);
     res.json(json);
   })
   .post(async (req, res) => {
-    console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
-    res.json({facilities: dataStore});
+    // console.log('Hello World');
+    res.send('Hello World');
+    // console.log('Form data in req.body', req.body);
+    // res.json({facilities: dataStore});
   });
 
 app.listen(port, async () => {
